16fafb2c495b012bf54ff8ceb3bfdca37ff05e51
Ubuntu <ubuntu@ip-172-31-22-217.ec2.internal>
2020-03-04 06:45:18 +0000

    addingCICD Ansible dir

---
+++ CICD/runsanity.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+TESTURL="localhost:8080/samplewar"
+
+
+launch_container()
+{
+  echo "INFO: Begin Docker container creation"
+  IMG=$1
+  BUILDDIR=$(pwd)
+  mkdir sanity
+
+  CID=$(/usr/bin/docker run -itd --name testc -p 8080:8080 -v ${BUILDDIR}/sanity:/opt/jboss/wildfly/standalone/log ${IMG})
+
+  if [ $? -ne "0" ]; then
+     echo "ERROR: Couldnt create Docker Container"
+     exit 1
+  else
+     sleep 15
+     echo "INFO: Container Launch Initiated ${CID}"
+  fi
+  
+  echo "INFO: Completed  Docker container creation"
+}
+
+#MAIN#
+launch_container $1
+
